a9dacb3da2ee9a868a9b3902ebbeaf6781fafdc5
AhmedBaghododa <125276501+Fyrnx@users.noreply.github.com>
2023-05-10 04:32:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-let fs = require('fs')
-let path = require('path')
 let http = require('http')
 let https = require('https')
 
-let url = 'https://st.depositphotos.com/1005676/2513/i/950/depositphotos_25134583-free-stock-photo-computer-background-http-web-page.jpg'
-
 async function GetImage(url) {
-    return await new Promise((resolve,reject) =>  {
+    return await new Promise(resolve =>  {
         let protocol = http
         if(/https.*/ig.test(url)) {protocol = https}
         protocol.get(url,res => {
             let raw = []
             res.on('data',chunk => {
                 raw.push(chunk)
-            })s
+            })
             res.on('end',_ => {
-                if(raw.length > 0) {
                     raw = Buffer.concat(raw)
                     resolve(raw)
-                } else { 
-                    reject()
-                }
             })
         })
     })
@@ -36,12 +28,7 @@ let server = http.createServer((req,res) => {
             res.write(data)
             res.end()
         })
-        .catch(err => { 
-            res.write('url isn\'t write')
-            res.end()
-        })
     }
 })
 
 server.listen(1000)
-console.log(`listing to port 1000 ...`);
