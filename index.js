6df5abfa7911d9dd681fe56c14347de2ae8c1cbf
AhmedBaghododa <125276501+Fyrnx@users.noreply.github.com>
2023-05-13 21:20:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let http = require('http')
 let https = require('https')
 
-async function GetImage(url) {
+async function GetUrl(url) {
     return await new Promise((resolve,reject) =>  {
         let protocol = http
         if(/https.*/ig.test(url)) {protocol = https}
@@ -20,6 +20,18 @@ async function GetImage(url) {
     })
 }
 
+async function CheckUrl(url) {
+    return await new Promise((resolve,reject) =>  {
+        let protocol = http
+        if(/https.*/ig.test(url)) {protocol = https}
+            protocol.get(url,_ => {
+                resolve(true)
+            }).on("error", (err) => {
+                resolve(false)
+            });
+    })
+}
+
 let server = http.createServer((req,res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     if(req.url == '/') { 
@@ -29,7 +41,7 @@ let server = http.createServer((req,res) => {
         let url = new URL(`http://${req.url}`)
         let q = url.searchParams.get('q')
 
-        GetImage(q).then(data => {
+        GetUrl(q).then(data => {
             res.write(data)
             res.end()
         }, err => {
@@ -37,6 +49,14 @@ let server = http.createServer((req,res) => {
             res.end()
         })
 
+    } else if(/\/check/.test(req.url)) { 
+        let url = new URL(`http://${req.url}`)
+        let q = url.searchParams.get('q')
+
+        CheckUrl(q).then(result => { 
+            res.write(result)
+            res.end()
+        })
     } else { 
         res.write(`there is not ${req.url.slice(1)} please use (read / check)`)
         res.end()
